63fae4f32ba7bec2f60bef81218c27fa04976944
foglet15 <emdort@gmail.com>
2021-04-03 09:08:33 -0400

    version 1.0

--- cryptPack.py
+++ cryptPack.py
@@ -4,13 +4,14 @@ import logging
 from cryptUI import *
 from PySide2 import QtCore, QtGui, QtWidgets
 
-FORMAT = logging.Formatter('%(asctime)-15s %(thread)x:: %(message)s')
+FORMAT = logging.Formatter('%(asctime)s %(thread)x:: %(message)s')
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(FORMAT)
 log.addHandler(ch)
+log.disabled = True
 
 class Vigenre_Method:
     def init(self):
@@ -28,12 +29,12 @@ class Vigenre_Method:
             key_char = ord(key[key_index])
             
             if method:
-                output_char = ((plain_char-32) + (key_char-32)) % 95
+                output_char = ((plain_char-32) + (key_char-32)) % 94
                 output_char += 32
             else:
-                output_char = ((plain_char-32)-(key_char-32)) % 95
+                output_char = ((plain_char-32)-(key_char-32)) % 94
                 output_char += 32
-            if output_char < 32 or output_char > 127:
+            if output_char < 32 or output_char > 126:
                 log.error(f"Error: output was a control character {output_char}")
             output += chr(output_char)
             log.debug(f"add plain char {plain_char} and key char {key_char} and got {output_char}")
@@ -47,7 +48,32 @@ class Controller:
         self.Vigenere = Vigenre_Method()
 
     def init_UI(self):
+        self.ui.key_len_spinBox.setRange(0,20000)
+        self.ui.input_textEdit.textChanged.connect(self.update_key_range)
+        self.ui.max_pushButton.pressed.connect(self.spin_to_max_len)
         self.ui.cipher_exec.pressed.connect(self.execute_method)
+        self.ui.gen_key_pushButton.pressed.connect(self.key_generator)
+
+    def spin_to_max_len(self):
+        length = len(self.ui.input_textEdit.toPlainText())
+        self.ui.key_len_spinBox.setValue(length)
+
+    def update_key_range(self):
+        max_leng = len(self.ui.input_textEdit.toPlainText())
+        self.ui.key_len_spinBox.setRange(0,max_leng)
+
+    def key_generator(self):
+        return_key = ''
+        log.info("randomly generating key")
+        if self.ui.key_len_spinBox.value() < 1:
+            return
+        for i in range(self.ui.key_len_spinBox.value()):
+            char = secrets.randbelow(93)
+            char += 32
+            return_key += chr(char)
+        if char < 32 or char > 126:
+            log.error(f"Error: output was a control character {output_char}")
+        self.ui.key_lineEdit.setText(return_key)
 
     def execute_method(self):
         plain_text = self.ui.input_textEdit.toPlainText()
